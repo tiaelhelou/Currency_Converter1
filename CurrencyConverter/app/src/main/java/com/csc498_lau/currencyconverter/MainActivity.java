dccc08484b4696c63901e023a2c043152bd9832e
tiahelou10 <tia.elhelou@outlook.com>
2022-02-04 23:34:10 +0200

    Commenting my code and finalizing the assignment

--- CurrencyConverter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity.java
+++ CurrencyConverter/app/src/main/java/com/csc498_lau/currencyconverter/MainActivity.java
@@ -1,3 +1,14 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                 *
+ * Name: Tia El Helou                                                              *
+ * Id: 202002154                                                                   *
+ * course: CSC498                                                                  *
+ * Date Last Modified: 4/2/2022                                                    *
+ * Program Description: An android app that convert USD to LBP and vice versa.     *
+ *                      Note that 1$ = 22,000 LBP rate                             *
+ *                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 package com.csc498_lau.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,11 +37,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void convert (View v) {
 
+        // Reading input from the user
         String input_usd_string = usd.getText().toString();
         String input_lbp_string = lbp.getText().toString();
 
         try {
 
+            // If the user want to convert from USD to LBP
             if ( (input_lbp_string.equals("") || input_lbp_string.equals("0")) && (!input_usd_string.equals("") && !input_usd_string.equals("0")) ) {
 
                 int input_usd = Integer.parseInt(input_usd_string);
@@ -39,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
 
                 String result = input_usd + " USD = " + usd_to_lbp + " LBP";
 
-                output.setText(result);
+                output.setText(result); // Changing the TextView to the result of the conversion
             }
 
+            // If the user want to convert from LBP to USD
             else if ( (input_usd_string.equals("") || input_usd_string.equals("0")) && (!input_lbp_string.equals("") && !input_lbp_string.equals("0")) ) {
 
                 int input_lbp = Integer.parseInt(input_lbp_string);
@@ -50,18 +64,26 @@ public class MainActivity extends AppCompatActivity {
 
                 String result = input_lbp + " LBP = " + lbp_to_usd + " USD";
 
-                output.setText(result);
+                output.setText(result); // Changing the TextView to the result of the conversion
             }
 
+            // If the user did not input any number
             else if ( (input_lbp_string.equals("") || input_lbp_string.equals("0")) && (input_usd_string.equals("0") || input_usd_string.equals("")) ) {
+
+                // Printing an message on the screen
                 Toast.makeText(getApplicationContext(), "You need to enter a number in one of the fields above!", Toast.LENGTH_LONG).show();
             }
 
+            // If the user input numbers in the two EditText (USD and LBP)
             else {
+
+                // Printing an message on the screen
                 Toast.makeText(getApplicationContext(), "Please enter a number in only one of the fields above!", Toast.LENGTH_LONG).show();
             }
 
-        } catch (Exception e) {
+        } catch (NumberFormatException e) { //Catching the error if the user input a character instead of a number
+
+            // Printing an message on the screen
             Toast.makeText(getApplicationContext(), "Please enter a valid number!", Toast.LENGTH_LONG).show();
         }
     }
